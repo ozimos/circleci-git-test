473f9db8b832a4bb65324e403a406e2487d9639a
Tovieye Moses Ozi <tovieye.ozi@gmail.com>
2020-04-21 09:08:20 +0100

    use only full sha1 in revision range

--- .circleci/circle_test.sh
+++ .circleci/circle_test.sh
@@ -1,8 +1,9 @@
 PACKAGE_PATH="./packages" 
-LATEST_PACKAGE_COMMIT_SINCE_DEVELOP=$(git rev-list -1 develop..$CIRCLE_SHA1 -- ${PACKAGE_PATH#./})
-LATEST_COMMIT_SINCE_DEVELOP=$(git rev-list -1 develop..$CIRCLE_SHA1)
-COMMITS_SINCE_DEVELOP=$(git rev-list develop..$CIRCLE_SHA1)
-COMMITS_COUNT_SINCE_DEVELOP=$(git rev-list --count develop..$CIRCLE_SHA1)
+DEVELOP_SHA1=$(git rev-parse origin/develop)
+LATEST_PACKAGE_COMMIT_SINCE_DEVELOP=$(git rev-list -1 $DEVELOP_SHA1..$CIRCLE_SHA1 -- ${PACKAGE_PATH#./})
+LATEST_COMMIT_SINCE_DEVELOP=$(git rev-list -1 $DEVELOP_SHA1..$CIRCLE_SHA1)
+COMMITS_SINCE_DEVELOP=$(git rev-list $DEVELOP_SHA1..$CIRCLE_SHA1)
+COMMITS_COUNT_SINCE_DEVELOP=$(git rev-list --count $DEVELOP_SHA1..$CIRCLE_SHA1)
 COMMITS_COUNT=$(git rev-list --count HEAD)
 echo -e "\e[36m  first changed commit in package since develop $LATEST_PACKAGE_COMMIT_SINCE_DEVELOP \e[0m"
 echo -e "\e[36m  first changed commit since develop $LATEST_COMMIT_SINCE_DEVELOP \e[0m"
@@ -10,3 +11,4 @@ echo -e "\e[36m  commits since develop $COMMITS_SINCE_DEVELOP \e[0m"
 echo -e "\e[36m  commits count since develop $COMMITS_COUNT_SINCE_DEVELOP \e[0m"
 echo -e "\e[36m  commits count $COMMITS_COUNT \e[0m"
 echo -e "\e[36m  this commit $CIRCLE_SHA1 \e[0m"
+echo -e "\e[36m  sha1 at develop HEAD $DEVELOP_SHA1 \e[0m"
